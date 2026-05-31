ab30aee30e2b232e559ffb70422f7aec0a728e88
Thipakan <106869925+Thipakan@users.noreply.github.com>
2026-05-31 15:44:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ export default function App() {
         <Route path="/docker" element={<Docker />} />
         <Route path="/haproxy" element={<Haproxy />} />
         <Route path="/wazuh" element={<Wazuh />} />
-        <Route path="/packet-tracer" element={<PacketTracer />} />
+        <Route path="/packettracer" element={<PacketTracer />} />
         <Route path="/kasa" element={<Kasa />} />
         <Route path="/kanap" element={<Kanap />} />
         <Route path="/mediatek" element={<Mediatek />} />
